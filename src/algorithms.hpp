b6a78a109972ffc0cb48986b7e894ecb47b91856
Hung Dang <hdang@athenahealth.com>
2018-02-22 10:38:14 -0500

    Add a FileReader class that can be used with different buffer parser and does not have any performance penalty.

---
+++ src/algorithms.hpp
@@ -0,0 +1,44 @@
+#pragma once
+
+#include "unistd.h"
+#include <sstream>
+#include <string>
+
+namespace scribe {
+    // A struct that read file content in fixed size chunks and parse them to a parser.
+    template <size_t BUFFER_SIZE, typename Parser> class FileReader {
+      public:
+        Parser operator()(const char *datafile, Parser &&parser) {
+            char read_buffer[BUFFER_SIZE + 1];
+            int fd = ::open(datafile, O_RDONLY);
+			
+            // Check that we can open a given file.
+            if (fd < 0) {
+                std::stringstream writer;
+                writer << "Cannot open file \"" << datafile << "\"";
+                throw(std::runtime_error(writer.str()));
+            }
+
+            // Read data into a string
+            while (true) {
+                auto nbytes = ::read(fd, read_buffer, BUFFER_SIZE);
+                if (nbytes < 0) {
+                    std::stringstream writer;
+                    writer << "Cannot read file \"" << datafile << "\"";
+                    throw(std::runtime_error(writer.str()));
+                };
+
+                // Parse read_buffer to get some useful information.
+                parser(read_buffer, read_buffer + nbytes);
+
+                // Stop if we reach the end of file.
+                if (nbytes != static_cast<decltype(nbytes)>(BUFFER_SIZE)) { break; };
+            }
+
+            // Close our file.
+            ::close(fd);
+
+            return parser;
+        }
+    };
+} // namespace scribe
