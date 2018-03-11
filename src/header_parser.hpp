acc6d62b315ea73a83c4f5391eac2ebd54abe390
Hung Dang <hungptit@gmail.com>
2018-03-11 11:36:48 -0400

    Finish a scribe header parser

--- src/header_parser.hpp
+++ src/header_parser.hpp
@@ -1,13 +1,18 @@
 #pragma once
-#include <stdlib.h>
-#include <time.h>
+#include <cstdlib>
+#include <ctime>
+#include <cstring>
+#include <algorithm>
 
 namespace scribe {
+    // A scribe header has this format "[03/08/2018 12:00:00 node1234.example.com generic.workqueue 123456]"
     struct MessageHeader {
-        std::time_t timestamp; // Timestamp: 03/08/2018 23::00:50
-        char server[32];       // A server address: job1120.domain_name.com
-        char pool[40];         // A job pool name i.e job.pool.name
-        int pid;			   // This is a process id
+        static constexpr size_t SERVER_BUFFER_SIZE = 32;
+        static constexpr size_t POOL_BUFFER_SIZE = 48;
+        std::time_t timestamp;           // Timestamp: 03/08/2018 23::00:50
+        char server[SERVER_BUFFER_SIZE]; // A server address: job1120.domain_name.com
+        char pool[POOL_BUFFER_SIZE];     // A job pool name i.e job.pool.name
+        int pid;                         // This is a process id
     };
 
     // Parse timestamp.
@@ -38,7 +43,7 @@ namespace scribe {
             ptr += 3;
             tm.tm_sec = parse_two_digits(ptr);
 
-			tm.tm_isdst = 0;
+            tm.tm_isdst = 0;
             return mktime(&tm);
         }
 
@@ -65,7 +70,7 @@ namespace scribe {
     class MessageHeaderParser {
       public:
         MessageHeader operator()(const char *begin, const char *end) {
-            assert((end - begin) > 32);
+            assert((begin + 32) < end);
             const char *ptr = begin + 1;
             MessageHeader header;
             header.timestamp = time_parser(ptr);
@@ -74,16 +79,18 @@ namespace scribe {
             // Parse server name
             const char *pos = static_cast<const char *>(memchr(ptr, SPACE, end - ptr));
             assert(pos != NULL);
-            memcpy(header.server, ptr, pos - ptr);
-            ptr = pos + 1;
+            memcpy(header.server, ptr, std::min<size_t>(pos - ptr, MessageHeader::SERVER_BUFFER_SIZE));
 
-            // Parse pool information
+            // Parse pool name.
+            ptr = pos + 1;
+            assert(ptr < end);
             pos = static_cast<const char *>(memchr(ptr, SPACE, end - ptr));
             assert(pos != NULL);
-            memcpy(header.pool, ptr, pos - ptr);
+            memcpy(header.pool, ptr, std::min<size_t>(pos - ptr, MessageHeader::POOL_BUFFER_SIZE));
 
-            // TODO: Do we need process id?
+            // Parse process id.
             ptr = pos + 1;
+            assert(ptr < end);
             header.pid = atoi(ptr);
 
             return header;
