5551c0b171d3b9670f78dc82a182518217127331
Hung Dang <hungptit@gmail.com>
2018-05-26 01:34:35 -0400

    Working on a fast grep command.

--- src/grep.hpp
+++ src/grep.hpp
@@ -56,5 +56,57 @@ namespace fastgrep {
         std::string linebuf;
     };
 
+    class GrepPolicyNew {
+      public:
+        GrepPolicyNew(const std::string &patt) : matcher(patt) {}
+
+        ~GrepPolicyNew() { process_linebuf(); }
+
+        void process(const char *begin, const size_t len) {
+            const char *start = begin;
+            const char *end = begin + len;
+            const char *ptr = begin;
+            while ((ptr = static_cast<const char *>(memchr(ptr, EOL, end - ptr)))) {
+                if (linebuf.empty()) {
+                    process_line(start, ptr - start);
+                } else {
+                    linebuf.append(start, ptr - start + 1);
+                    process_linebuf();
+                    linebuf.clear();
+                }
+
+                // Update parameters
+                start = ++ptr;
+                ++lines;
+
+                // Stop if we reach the end of the buffer.
+                if (start == end) break;
+            }
+
+            // Update the line buffer with leftover data.
+            if (start != end) { linebuf.append(start, end - start); }
+            pos += len;
+        }
+
+        size_t lines = 0;
+        size_t pos = 0;
+        std::string linebuf;
+        utils::hyperscan::RegexMatcher matcher;
+
+      protected:
+        void process_line(const char *begin, const size_t len) {
+            if (matcher.is_matched(begin, len)) {
+                fmt::print("{}\n", std::string(begin, begin + len));
+            }
+        }
+
+        void process_linebuf() {
+            if (matcher.is_matched(linebuf.data(), linebuf.size())) {
+                fmt::print("{}\n", linebuf);
+            }
+        }
+
+    };
+
     struct ExactMatchPolicy {};
 } // namespace fastgrep
