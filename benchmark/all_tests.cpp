54fbd0ecfb6a7348ca7719f60f79242c58a9cd03
Hung Dang <hungptit@gmail.com>
2018-06-05 22:30:22 -0400

    Fix issue in Linux.

--- benchmark/all_tests.cpp
+++ benchmark/all_tests.cpp
@@ -13,7 +13,6 @@ constexpr int number_of_operations = 1;
 CELERO_MAIN
 
 const std::string datafile("3200.txt");
-const std::string logfile("grep_data.log");
 const std::vector<std::string> patterns = {"Twain",
                                            "(?i)Twain",
                                            "[a-z]shing",
@@ -21,11 +20,6 @@ const std::vector<std::string> patterns = {"Twain",
                                            "\b\\w+nn\b",
                                            "[a-q][^u-z]{13}x",
                                            "Tom|Sawyer|Huckleberry|Finn"};
-
-const std::vector<std::string> log_patterns = {
-    "Starting execution",  "p4pqmewebsync.*finished in (\\d)*",
-    "\"LEVEL\":\"error\"", "job1070.athenahealth.com",
-    "db.db7.urgent",       "Execution error. Attempting to handle"};
 
 namespace {
     void run_a_test(const std::string datafile, const std::string &command,
@@ -40,7 +34,7 @@ namespace {
 } // namespace
 
 BASELINE(mark_twain, grep_brew, number_of_samples, number_of_operations) {
-    run_all_tests(datafile, "ggrep -E", patterns);
+    run_all_tests(datafile, "grep -E", patterns);
 }
 
 BENCHMARK(mark_twain, ag, number_of_samples, number_of_operations) {
@@ -57,26 +51,4 @@ BENCHMARK(mark_twain, fgrep, number_of_samples, number_of_operations) {
 
 BENCHMARK(mark_twain, fastgrep, number_of_samples, number_of_operations) {
     run_all_tests(datafile, "../commands/fastgrep", patterns);
-}
-
-// Benchmark log search
-constexpr int log_search_samples = 3;
-BASELINE(logdata, grep_brew, log_search_samples, number_of_operations) {
-    run_all_tests(logfile, "ggrep -E", log_patterns);
-}
-
-BENCHMARK(logdata, ag, log_search_samples, number_of_operations) {
-    run_all_tests(logfile, "ag", log_patterns);
-}
-
-BENCHMARK(logdata, ripgrep, log_search_samples, number_of_operations) {
-    run_all_tests(logfile, "rg", log_patterns);
-}
-
-BENCHMARK(logdata, fgrep, log_search_samples, number_of_operations) {
-    run_all_tests(logfile, "../commands/fgrep", log_patterns);
-}
-
-BENCHMARK(logdata, fastgrep, log_search_samples, number_of_operations) {
-    run_all_tests(logfile, "../commands/fastgrep", log_patterns);
 }
