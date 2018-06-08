996dff79610a2c72ff2c241485b990af5206745d
Hung Dang <hungptit@gmail.com>
2018-06-08 00:00:34 -0400

    Working on demos for fgrep.

--- commands/fgrep.cpp
+++ commands/fgrep.cpp
@@ -5,10 +5,19 @@
 #include <algorithm>
 #include <iostream>
 #include <string>
+#include "utils/regex_matchers.hpp"
+#include "utils/matchers.hpp"
+#include "utils/matchers_avx2.hpp"
 
 int main(int argc, char *argv[]) {
     if (argc < 3) return EXIT_FAILURE;
-    using Reader = ioutils::FileReader<fastgrep::GrepPolicyNew, 1 << 20>;
+
+    // Matchers
+    using Matcher1 = utils::avx2::ContainsStd;
+    using Matcher2 = utils::avx2::Contains;
+    using Matcher3 = utils::hyperscan::RegexMatcher;
+
+    using Reader = ioutils::FileReader<fastgrep::GrepPolicy<Matcher2>, 1 << 17>;
     Reader grep(argv[1]);
     for (auto idx = 2; idx < argc; ++idx) { grep(argv[idx]); }
     return EXIT_SUCCESS;
