5564b7907900aa925606807b144601129919444d
Hung Dang <hdang@athenahealth.com>
2018-12-05 23:53:30 -0500

    Update the benchmark for memory mapped and streaming reader

--- benchmark/all_tests.cpp
+++ benchmark/all_tests.cpp
@@ -45,12 +45,9 @@ namespace {
 } // namespace
 
 BASELINE(mark_twain, grep_brew, number_of_samples, number_of_operations) {
-    run_all_tests(datafile, "grep -E", patterns);
+    run_all_tests(datafile, "grep -P", patterns);
 }
 
-BENCHMARK(mark_twain, fgrep_warm_up, number_of_samples, number_of_operations) {
-    run_all_tests(datafile, "../commands/fgrep", patterns);
-}
 
 BENCHMARK(mark_twain, ag, number_of_samples, number_of_operations) {
     run_all_tests(datafile, "ag", patterns);
@@ -60,6 +57,10 @@ BENCHMARK(mark_twain, ripgrep, number_of_samples, number_of_operations) {
     run_all_tests(datafile, "rg", patterns);
 }
 
-BENCHMARK(mark_twain, fgrep, number_of_samples, number_of_operations) {
-    run_all_tests(datafile, "../commands/fgrep", patterns);
+BENCHMARK(mark_twain, fgrep_mmap, number_of_samples, number_of_operations) {
+    run_all_tests(datafile, "../commands/fgrep --mmap ", patterns);
+}
+
+BENCHMARK(mark_twain, fgrep_stream, number_of_samples, number_of_operations) {
+    run_all_tests(datafile, "../commands/fgrep --stream ", patterns);
 }
