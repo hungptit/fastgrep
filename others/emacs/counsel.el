b1c562c388cc66915832a86d62e3ab6ddd04698e
Hung Dang <hdang@athenahealth.com>
2019-05-03 17:52:56 -0400

    Add a sample LISP code that can be used to integrate fast-grep, codesearch, and fast-locate into Emacs.

---
+++ others/emacs/counsel.el
@@ -0,0 +1,58 @@
+;;** `counsel-fast-grep'
+(defcustom counsel-fast-grep-base-command "/Users/hdang/bin/fast-grep -n -p '[.](c|cc|cpp|h|hpp|hh|md|org|txt)$' %s ."
+  "Alternative to `counsel-ag-base-command' using fast-grep"
+  :type 'string)
+
+(counsel-set-async-exit-code 'counsel-fast-grep 1 "No matches found")
+(ivy-set-occur 'counsel-fast-grep 'counsel-ag-occur)
+(ivy-set-display-transformer 'counsel-fast-grep 'counsel-git-grep-transformer)
+
+;;;###autoload
+(defun counsel-fast-grep (&optional initial-input initial-directory extra-rg-args rg-prompt)
+  "Grep for a string in the current directory using fast-grep."
+  (interactive)
+  (let ((counsel-ag-base-command counsel-fast-grep-base-command)
+        (counsel--grep-tool-look-around
+         (let ((rg (car (split-string counsel-fast-grep-base-command)))
+               (switch "--pcre2"))
+           (and (eq 0 (call-process rg nil nil nil switch "--version"))
+                switch))))
+    (counsel-ag initial-input initial-directory extra-rg-args rg-prompt
+                :caller 'counsel-fast-grep)))
+(cl-pushnew 'counsel-fast-grep ivy-highlight-grep-commands)
+
+;;** `counsel-codesearch'
+(defcustom counsel-codesearch-base-command "/Users/hdang/bin/codesearch -n -p '[.](c|cc|cpp|h|hpp|hh|md|org|txt)$' %s -d /Users/hdang/.codesearch"
+  "Search for a text pattern using codeseach command."
+  :type 'string)
+
+(counsel-set-async-exit-code 'counsel-codesearch 1 "No matches found")
+(ivy-set-occur 'counsel-codesearch 'counsel-ag-occur)
+(ivy-set-display-transformer 'counsel-codesearch 'counsel-git-grep-transformer)
+
+;;;###autoload
+(defun counsel-codesearch (&optional initial-input initial-directory extra-rg-args rg-prompt)
+  "Grep for a string in the current directory using codesearch."
+  (interactive)
+  (let ((counsel-ag-base-command counsel-codesearch-base-command)
+        (counsel--grep-tool-look-around
+         (let ((rg (car (split-string counsel-codesearch-base-command)))
+               (switch "--pcre2"))
+           (and (eq 0 (call-process rg nil nil nil switch "--version"))
+                switch))))
+    (counsel-ag initial-input initial-directory extra-rg-args rg-prompt
+                :caller 'counsel-codesearch)))
+(cl-pushnew 'counsel-codesearch ivy-highlight-grep-commands)
+
+;; For fast-locate command
+(defun counsel-locate-cmd-default (input)
+  "Return a shell command based on INPUT."
+  (counsel-require-program "/Users/hdang/bin/fast-locate")
+  (format "/Users/hdang/bin/fast-locate '%s' -d /Users/hdang/.database"
+          (counsel--elisp-to-pcre
+           (ivy--regex input))))
+
+(defun counsel-locate-cmd-noregex (input)
+  "Return a shell command based on INPUT."
+  (counsel-require-program "locate")
+  (format "/Users/hdang/bin/fast-locate -d /Users/hdang/.database '%s'" input))
