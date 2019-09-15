4ace42b9f24a67551da8416cd0546699c952af47
Hung Dang <hdang@tableau.com>
2019-09-15 08:45:38 -0700

    Change the 3p folder.

---
+++ 3p/build_all.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -e                          # Use Bash strict mode
+
+printf "Build doctest\n";
+./build_using_cmake.sh doctest "-DCMAKE_CXX_COMPILER=clang++" > /dev/null
+
+printf "Build FMT\n"
+./build_using_cmake.sh fmt "-DFMT_DOC=OFF -DFMT_TEST=OFF" > /dev/null
+
+printf "Build benchmark\n"
+./build_using_cmake.sh benchmark -DBENCHMARK_ENABLE_TESTING=OFF CMAKE_CXX_COMPILER=clang++ > /dev/null
+
+printf "Setup Clara\n"
+cp src/Clara/include/*.hpp include/
+
+printf "Build Celero\n"
+./build_using_cmake.sh Celero "-DCELERO_COMPILE_DYNAMIC_LIBRARIES:BOOL=OFF -DCELERO_ENABLE_EXPERIMENTS:BOOL=OFF" > /dev/null
+
+printf "Build utils"
+pushd src/utils/
+./install.sh ../../
+popd
+
+printf "Build ioutils"
+pushd src/ioutils/
+./install.sh ../../
+popd
+
+printf "Build hyperscan"
+./build_using_cmake.sh hyperscan
